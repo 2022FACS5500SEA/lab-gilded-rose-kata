ce5a14b5b2efe04eb76d693343c5122634d3dd4e
Mark Bonicillo <markabonicillo@gmail.com>
2022-11-01 15:44:26 -0700

    Rename test class

--- src/test/java/com/gildedrose/GildedRoseTests.java
+++ src/test/java/com/gildedrose/GildedRoseTests.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class GildedRoseTest {
+class GildedRoseTests {
 
     private Item[] items;
     private GildedRose gildedRose;
